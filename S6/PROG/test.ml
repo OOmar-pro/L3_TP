51d9af0f7d6a70918adb0877c8c3d84f30e45166
kage <laptop@kage7340.com>
2020-08-04 18:27:07 +0200

    last

---
+++ S6/PROG/test.ml
@@ -0,0 +1,25 @@
+(*
+module ListZipper2 : LIST_ZIPPER = struct
+  type 'a t = 'a list * 'a list
+
+  exception First
+
+  exception Last
+
+  exception Empty
+
+  let create () = ([], [])
+
+  let previous z = match z with [], _ -> raise First | h :: t, l -> (t, h :: l)
+
+  let next z = match z with _, [] -> raise Last | l, h :: t -> (h :: l, t)
+
+  let insert_weird (l, r) a = (l, a :: r) (* what happens?*)
+
+  let insert (l, r) a = (a :: l, r)
+
+  let remove z = match z with _, [] -> raise Empty | l, h :: t -> (l, t)
+end
+*)
+
+open Base;;
